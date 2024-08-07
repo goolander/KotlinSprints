1fcd0a8a879c9b13cfd40bf8c20746b83bd5fc19
goolander <glyanenkodenis@gmail.com>
2024-08-07 03:04:46 +0300

    KS-1-4 add info gagarin space

--- src/main/kotlin/lesson_1/lesson1_task4.kt
+++ src/main/kotlin/lesson_1/lesson1_task4.kt
@@ -7,6 +7,7 @@ fun main() {
     val seconds: Short = 6480
     val partOfYear: Double = 2.0547945205479453E-4
     val apogee: Int = 327000
+
     print(length)
     println(age)
     println(partOfDay)
