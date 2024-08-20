2c1a5b6e4efc6b3af236925811f2d436c5a13bca
goolander <glyanenkodenis@gmail.com>
2024-08-21 02:49:03 +0300

    KS-2-1 update name var

--- src/main/kotlin/lesson_2/lesson2_task1.kt
+++ src/main/kotlin/lesson_2/lesson2_task1.kt
@@ -1,10 +1,12 @@
 package org.example.lesson_2
 
 fun main() {
-   var a = 3
-   var b = 4
-   var c = 3
-   var d = 5
-   var average: Float = (((a+b+c+d)/4).toFloat())
-    println((String.format("%.2f", average)))
+   var markStudent1 = 3
+   var markStudent2 = 4
+   var markStudent3 = 3
+   var markStudent4 = 5
+   var allStudents = 4
+   var averageMark: Float = (((markStudent1+markStudent2+markStudent3+markStudent4)
+           /allStudents).toFloat())
+    println((String.format("%.2f", averageMark)))
 }
