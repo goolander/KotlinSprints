afd40424ee1843546c97c7281af9791bbea2affc
goolander <glyanenkodenis@gmail.com>
2024-08-20 22:45:14 +0300

    KS-2-1 add fun average mark

---
+++ src/main/kotlin/lesson_2/lesson2_task1.kt
@@ -0,0 +1,10 @@
+package org.example.lesson_2
+
+fun main() {
+   var a = 3
+   var b = 4
+   var c = 3
+   var d = 5
+   var average: Float = (((a+b+c+d)/4).toFloat())
+    println((String.format("%.2f", average)))
+}
