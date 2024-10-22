33f02627237ea9443e6fa9333b9be68bf74d85d0
goolander <glyanenkodenis@gmail.com>
2024-10-23 00:47:08 +0300

    KS-2-4 add val convertBuff

--- src/main/kotlin/lesson_2/lesson2_task4.kt
+++ src/main/kotlin/lesson_2/lesson2_task4.kt
@@ -3,9 +3,10 @@ package lesson_2
 fun main() {
     var crystalMinerals = 7
     var ironMinerals = 11
-    val buff = 0.2
-    val buffCrystalMinerals = (buff * crystalMinerals).toInt()
-    val buffIronMinerals = (buff * ironMinerals).toInt()
+    val buff = 20
+    val convertBuff = buff / 10
+    val buffCrystalMinerals = (convertBuff * crystalMinerals).toInt()
+    val buffIronMinerals = (convertBuff * ironMinerals).toInt()
     println("Дополнительно получено $buffCrystalMinerals кристалической руды")
     println("Дополнительно получено $buffIronMinerals железеой руды")
 }
