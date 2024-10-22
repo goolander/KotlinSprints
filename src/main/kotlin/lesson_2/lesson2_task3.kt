dacd5f03803b72121ee5840f4439c41eb5122267
goolander <glyanenkodenis@gmail.com>
2024-10-23 00:02:34 +0300

    KS-2-3 delete code and add new value

--- src/main/kotlin/lesson_2/lesson2_task3.kt
+++ src/main/kotlin/lesson_2/lesson2_task3.kt
@@ -8,11 +8,11 @@ fun main() {
     var timeStart = "9:39"
     val timeStartWithLeadingZero = timeStart.padStart(5, '0')
     val formatterTimeStart = DateTimeFormatter.ofPattern("HH:mm")
-    //var formattedTimeStart = timeStartWithLeadingZero.format(formatterTimeStart)
     val formattedTimeStart = LocalTime.parse("$timeStartWithLeadingZero", formatterTimeStart)
     val convertFormattedTimeStart = formattedTimeStart.toSecondOfDay()
     var timeTravel = "457"
-    val convertTimeTravel = (timeTravel.toInt()) * 60
+    val converToSecond = 60
+    val convertTimeTravel = (timeTravel.toInt()) * converToSecond
     val timeFinishSec = (convertFormattedTimeStart + convertTimeTravel).seconds
     val timeFinish = timeFinishSec.toComponents { hours, minutes, seconds, _ ->
         "%02d:%02d".format(hours, minutes, seconds) }
