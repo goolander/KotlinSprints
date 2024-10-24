a459fae6dbf5144d6347ced3decfcba360f278e9
goolander <glyanenkodenis@gmail.com>
2024-10-25 00:07:11 +0300

    KS-2-5 bug fix and add val convertToDecimal

--- src/main/kotlin/lesson_2/lesson2_task5.kt
+++ src/main/kotlin/lesson_2/lesson2_task5.kt
@@ -4,6 +4,8 @@ fun main() {
     val principalAmount = 70000
     val interestRate = 16.7
     val timePeriod = 20
-    val compoundInterest = principalAmount.toDouble() * Math.pow((1+ interestRate/100.00),timePeriod.toDouble())- principalAmount
+    val convertToDecimal = 100
+    val compoundInterest = principalAmount.toDouble() * Math.pow((1 + interestRate
+            / convertToDecimal),timePeriod.toDouble())
     println((String.format("%.3f", compoundInterest)))
 }
