61487da9fc26937e14be6681c14e490e2f35ca44
goolander <glyanenkodenis@gmail.com>
2024-10-24 23:50:08 +0300

    KS-2-4 add val convertToDecimal

--- src/main/kotlin/lesson_2/lesson2_task4.kt
+++ src/main/kotlin/lesson_2/lesson2_task4.kt
@@ -4,7 +4,8 @@ fun main() {
     var crystalMinerals = 7
     var ironMinerals = 11
     val buff = 20
-    val convertBuff = buff / 10
+    val convertToDecimal = 10
+    val convertBuff = buff / convertToDecimal
     val buffCrystalMinerals = (convertBuff * crystalMinerals).toInt()
     val buffIronMinerals = (convertBuff * ironMinerals).toInt()
     println("Дополнительно получено $buffCrystalMinerals кристалической руды")
